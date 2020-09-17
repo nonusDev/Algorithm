f8e0b8947685cc870ad23f0a7e9bc3140fe751ba
nonusDev <nonus.dev@gmail.com>
2020-09-17 22:32:18 +0900

    Level1

---
+++ Programmers/Level1/시저암호.py
@@ -0,0 +1,21 @@
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+
+    return "".join(s)
+
+# 1
+s = "AB"
+n = 1
+
+# 2
+s = "z"
+n = 1
+
+# 3
+s = "a B z"
+n = 4
