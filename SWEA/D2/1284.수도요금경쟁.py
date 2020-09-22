3000d7d51b8f74a29547f28af61d3f5b1268d04e
nonusDev <nonus.dev@gmail.com>
2020-09-22 21:17:44 +0900

    D2

--- SWEA/D2/1284.수도요금경쟁.py
+++ SWEA/D2/1284.수도요금경쟁.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.stdin = open("1284.수도요금전쟁.txt", 'r')
+sys.stdin = open("1284.수도요금경쟁.txt", 'r')
 
 for tc in range(1, int(input())+1):
     P, Q, R, S, W = map(int, input().split())
