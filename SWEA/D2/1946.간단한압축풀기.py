4650cbc81e8e2a215865fb34270c780c68cee9b2
nonusDev <nonus.dev@gmail.com>
2020-10-22 00:55:47 +0900

    D2

--- SWEA/D2/1946.간단한압축풀기.py
+++ SWEA/D2/1946.간단한압축풀기.py
@@ -4,4 +4,21 @@ sys.stdin = open("1946.간단한압축풀기.txt", 'r')
 
 for tc in range(1, int(input())+1):
     N = int(input())
-    arr = [list(map(str, input().split())) for _ in range(N)]
+    res = ''
+    for i in range(N):
+        C, K = input().split()
+        
+        res += C * int(K)
+    print(f"#{tc}")
+    # 1
+    for i in range(0, len(res), 10):
+        print(res[i:i+10])
+
+    # # 2
+    # cnt = 1
+    # for i in res:
+    #     print(i, end='')
+    #     if cnt % 10 == 0:
+    #         print()
+    #     cnt += 1
+    # print()
