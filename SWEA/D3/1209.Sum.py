327059f8c2b12401e31e13f71055818545961754
nonusDev <nonus.dev@gmail.com>
2020-10-27 17:10:51 +0900

    D3

--- SWEA/D3/1209.Sum.py
+++ SWEA/D3/1209.Sum.py
@@ -2,22 +2,27 @@ import sys
 
 sys.stdin = open("1209.Sum.txt", 'r')
 
-for tc in range(1, int(input())+1):
+T = 10
+for t in range(1, T+1):
+    tc = int(input())
     arr = [list(map(int, input().split())) for _ in range(100)]
-
+    
     res = 0
     for i in range(len(arr)):
         c_sum = 0
         r_sum = 0
-        d_sum = 0
-        rd_sum = 0 
         for j in range(len(arr)):
             c_sum += arr[i][j]
             r_sum += arr[j][i]
-            d_sum += arr[i][i]
-            rd_sum += arr[i][len(arr)-1-i]
+        if res < max(c_sum, r_sum):
+            res = max(c_sum, r_sum)
     
-    if res < max(c_sum, r_sum, d_sum, rd_sum):
-        res = max(c_sum, r_sum, d_sum, rd_sum)
-    
+    d_sum = 0
+    rd_sum = 0
+    for i in range(len(arr)):
+        d_sum += arr[i][i]
+        rd_sum += arr[i][len(arr)-i-1]
+    if res < max(d_sum, rd_sum):
+        res = max(d_sum, rd_sum)
+
     print(f"#{tc} {res}")
