467f1f1c32c2bfdf8ed2a310bc827d250646c25d
nonusDev <nonus.dev@gmail.com>
2020-10-29 18:03:55 +0900

    README

--- SWEA/D3/3431.준환이의운동관리.py
+++ SWEA/D3/3431.준환이의운동관리.py
@@ -4,4 +4,11 @@ sys.stdin = open("3431.준환이의운동관리.txt", 'r')
 
 for tc in range(1, int(input())+1):
     L, U, X = map(int, input().split())
-    print(L)
+    
+    res = 0
+    if X <= L:
+        res = L - X
+    elif U <= X:
+        res = -1
+    
+    print(f"#{tc} {res}")
